52f789e2b49a97e8f4c9d26eed0927f4d8e1a973
hari <ganambihai10@gmail.com>
2024-12-17 16:36:24 +0530

    add functionality

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 from db import getTaskList
+from db import addTask
 app=Flask(__name__)
 
 # tasklist=[['walk dog',True],['shopping',True],['study python',False]]
-tasklist=getTaskList()
+
 
 @app.route('/')
 def home():
+    tasklist=getTaskList()
     return render_template('tasklist.html',TaskList=tasklist)
+
+@app.route('/add',methods=['POST'])
+def add():
+    taskname=request.form['taskName']
+    duedate=request.form['dueDate']
+    addTask(taskname,duedate)
+    return redirect(url_for('home'))
+
+
 
 
 app.run()
